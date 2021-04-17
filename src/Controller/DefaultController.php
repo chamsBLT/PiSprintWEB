2dfe612e8f7726a715f5e9965e5d4f6559093078
chamsBLT <61758859+chamsBLT@users.noreply.github.com>
2021-04-17 02:53:57 +0100

    .

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,11 +1,14 @@
 <?php
-
-
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
-class DefaultController
+class DefaultController extends AbstractController
 {
 
+    public function FrontPage(){
+        return $this->render('front.html.twig');
+    }
 }
