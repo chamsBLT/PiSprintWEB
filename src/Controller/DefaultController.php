512cf918c66828f2a60d73a12e684e34c55efbde
chamsBLT <61758859+chamsBLT@users.noreply.github.com>
2021-04-18 02:35:01 +0100

    .

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,7 +12,112 @@ class DefaultController extends AbstractController
         return $this->render('front/choicePage.html.twig');
     }
 
-    public function WorkoutF(){
-        return $this->render('front/muscleList.html.twig');
+    public function WorkoutList(){
+        return $this->render('front/workout/muscleList.html.twig');
+    }
+
+    public function ArmsWlist()
+    {
+        $sql = "select name,nbr_series,duree_serie,description from workout WHERE body_part LIKE 'arms'";
+
+        $em = $this->getDoctrine()->getManager();
+        $stmt = $em->getConnection()->prepare($sql);
+        $stmt->execute();
+
+        $data = $stmt->fetchAll();
+
+        return $this->render('front/workout/workoutByMuscle.html.twig', array(
+            'data' => $data,
+        ));
+    }
+
+    public function ShouldersWlist()
+    {
+        $sql = "select name,nbr_series,duree_serie,description from workout WHERE body_part LIKE 'shoulders'";
+
+        $em = $this->getDoctrine()->getManager();
+        $stmt = $em->getConnection()->prepare($sql);
+        $stmt->execute();
+
+        $data = $stmt->fetchAll();
+
+        return $this->render('front/workout/workoutByMuscle.html.twig', array(
+            'data' => $data,
+        ));
+    }
+
+    public function ChestWlist()
+    {
+        $sql = "select name,nbr_series,duree_serie,description from workout WHERE body_part LIKE 'chest'";
+
+        $em = $this->getDoctrine()->getManager();
+        $stmt = $em->getConnection()->prepare($sql);
+        $stmt->execute();
+
+        $data = $stmt->fetchAll();
+
+        return $this->render('front/workout/workoutByMuscle.html.twig', array(
+            'data' => $data,
+        ));
+    }
+
+    public function AbsWlist()
+    {
+        $sql = "select name,nbr_series,duree_serie,description from workout WHERE body_part LIKE 'abdominals'";
+
+        $em = $this->getDoctrine()->getManager();
+        $stmt = $em->getConnection()->prepare($sql);
+        $stmt->execute();
+
+        $data = $stmt->fetchAll();
+
+        return $this->render('front/workout/workoutByMuscle.html.twig', array(
+            'data' => $data,
+        ));
+    }
+
+    public function BackWlist()
+    {
+        $sql = "select name,nbr_series,duree_serie,description from workout WHERE body_part LIKE 'back'";
+
+        $em = $this->getDoctrine()->getManager();
+        $stmt = $em->getConnection()->prepare($sql);
+        $stmt->execute();
+
+        $data = $stmt->fetchAll();
+
+        return $this->render('front/workout/workoutByMuscle.html.twig', array(
+            'data' => $data,
+        ));
+    }
+
+    public function LegsWlist()
+    {
+        $sql = "select name,nbr_series,duree_serie,description from workout WHERE body_part LIKE 'legs'";
+
+        $em = $this->getDoctrine()->getManager();
+        $stmt = $em->getConnection()->prepare($sql);
+        $stmt->execute();
+
+        $data = $stmt->fetchAll();
+
+        return $this->render('front/workout/workoutByMuscle.html.twig', array(
+            'data' => $data,
+        ));
+    }
+
+    public function GlutesWlist()
+    {
+        $sql = "select name,nbr_series,duree_serie,description from workout WHERE body_part LIKE 'glutes'";
+
+        $em = $this->getDoctrine()->getManager();
+        $stmt = $em->getConnection()->prepare($sql);
+        $stmt->execute();
+
+        $data = $stmt->fetchAll();
+
+        return $this->render('front/workout/workoutByMuscle.html.twig', array(
+            'data' => $data,
+        ));
     }
 }
